969210de5d02fbe7c012b500a6e743c7def1c8a5
Alex Odysseus David <alexxodavid@gmail.com>
2025-06-16 15:00:33 -0400

    Exercise: Chef Marco needs a menu!

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+const menu = [
+  { id: 1, dish: "Baked Shrimp Scampi", price: 20 },
+  { id: 2, dish: "Chicken Parmigiana", price: 14 },
+  { id: 3, dish: "Margherita Pizza", price: 17 },
+  { id: 4, dish: "Penne with Vodka Sauce", price: 18 }
+];
+
+
 const express = require("express");
 const app = express();
 
@@ -7,4 +15,19 @@ app.get("/", function (req, res) {
 
 app.listen(8080, function () {
   console.log("Server is listening on port 8080");
+});
+
+app.get("/menu", (req, res) => {
+  res.json(menu);
+});
+
+app.get("/menu/:menuItem", (req, res) => {
+  const id = parseInt(req.params.menuItem);
+  const item = menu.find(d => d.id === id);
+
+  if (item) {
+    res.json(item);
+  } else {
+    res.status(404).send("Menu item not found");
+  }
 });
