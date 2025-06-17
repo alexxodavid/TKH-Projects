b97fb219b2b6f3cfbf5bb6dd750c3f2888f1ecec
Alex Odysseus David <alexxodavid@gmail.com>
2025-06-17 13:28:37 -0400

    Exercise: Chef Marco wants the search

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
+const express = require("express");
+const app = express();
+
 const menu = [
   { id: 1, dish: "Baked Shrimp Scampi", price: 20 },
   { id: 2, dish: "Chicken Parmigiana", price: 14 },
   { id: 3, dish: "Margherita Pizza", price: 17 },
   { id: 4, dish: "Penne with Vodka Sauce", price: 18 }
 ];
-
-
-const express = require("express");
-const app = express();
 
 app.get("/", function (req, res) {
   res.send("Welcome to Chef Marco's Italian Bistro!").end();
@@ -17,21 +16,27 @@ app.post("/reservations", (req, res) => {
   res.status(501).send("Route exists but isn’t implemented yet!");
 });
 
-app.listen(8080, function () {
-  console.log("Server is listening on port 8080");
-});
-
 app.get("/menu", (req, res) => {
-  res.json(menu);
+  const { maxPrice } = req.query;
+  if (!maxPrice) return res.json(menu);
+  const filteredMenu = menu.filter(item => item.price <= parseFloat(maxPrice));
+  if (filteredMenu.length === 0) {
+    return res.status(404).json({ error: "No menu items found under that price." });
+  }
+  res.json(filteredMenu);
 });
 
 app.get("/menu/:menuItem", (req, res) => {
   const id = parseInt(req.params.menuItem);
   const item = menu.find(d => d.id === id);
-
   if (item) {
     res.json(item);
   } else {
     res.status(404).send("Menu item not found");
   }
 });
+
+app.listen(8080, function () {
+  console.log("Server is listening on port 8080");
+});
+
